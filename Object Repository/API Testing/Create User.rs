633a23fd3566e8e864679bbb0e97e087a65b5fd6
rayudu-web <64154612+rayudu-web@users.noreply.github.com>
2020-04-23 00:13:32 +0530

    test

---
+++ Object Repository/API Testing/Create User.rs
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>Create User</name>
+   <tag></tag>
+   <elementGuidId>2347874e-99b8-414d-ad9e-3ea24e950d8b</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n    \&quot;name\&quot;: \&quot;${name}\&quot;,\n    \&quot;job\&quot;: \&quot;leader\&quot;\n}&quot;,
+  &quot;contentType&quot;: &quot;text/plain&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>text/plain</value>
+   </httpHeaderProperties>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>${url}/api/users</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <variables>
+      <defaultValue>'Testuser'</defaultValue>
+      <description></description>
+      <id>e837701a-1483-42a1-8762-89b608d0bbcf</id>
+      <masked>false</masked>
+      <name>Name</name>
+   </variables>
+   <variables>
+      <defaultValue>GlobalVariable.Endpoint</defaultValue>
+      <description></description>
+      <id>c79113c5-3392-4494-8f2a-171d7e9ca0ae</id>
+      <masked>false</masked>
+      <name>url</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+
+
+GlobalVariable.variable
+
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+
+def variables = request.getVariables()
+def variable = variables.get('yourVariableName')</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Test Suites/UserTestSuite.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>UserTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>96b65e55-04de-4ffd-9771-84c2805b409c</testSuiteGuid>
+   <testCaseLink>
+      <guid>e15cf79c-8303-4242-94b0-7091b11ee565</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UserDetailsVerification/DetailsVerification</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>18fdc39d-8812-4af8-b304-62c6754077bf</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
