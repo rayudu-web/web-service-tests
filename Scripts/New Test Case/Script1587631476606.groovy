e9af9f7981d26b96336b4cf3a3766b481538bfac
rayudu-web <64154612+rayudu-web@users.noreply.github.com>
2020-04-23 14:17:15 +0530

    test1

--- Scripts/New Test Case/Script1587631476606.groovy
+++ Scripts/New Test Case/Script1587631476606.groovy
@@ -15,7 +15,3 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 
-Response = WS.sendRequest(findTestObject('API Testing/Create User'))
-
-WS.verifyResponseStatusCode(Response, 201)
-
